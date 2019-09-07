8552a5fa1fb8665e8a774d257854a1f716a99222
alumno <alumno>
2019-09-06 22:19:34 -0300

    Clase 5

---
+++ Clase 5/Array/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mostrarVectorInt(int vec[], int tam);
+int obtenerMayor(int vec[], int tam);
+
+
+int main()
+{
+    int numeros[5];
+    int suma = 0;
+    int max;
+    int index;
+
+    for(int i = 0; i < 5; i++){
+        printf("Ingrese un numero: ");
+        scanf("%d", &numeros[i]);
+    }
+    mostrarVectorInt(numeros, 5);
+    /*
+    for(int i = 0; i < 5; i++){
+        printf(" %d ", numeros[i]);
+        if(max <= numeros[i]){
+                max = numeros[i];
+                index = i;
+            }
+    }*/
+    max = obtenerMayor(numeros, 5);
+
+    for(int i = 0; i < 5; i++){
+        suma += numeros[i];
+    }
+        printf("\n la suma es: %d", suma);
+
+         printf("\n el maximo es %d y el indice es %d", max, index);
+
+    return 0;
+}
+
+void mostrarVectorInt(int vec[], int tam){
+    for(int i=0; i<tam; i++){
+        printf("%d ", vec[i]);
+
+    }
+    printf("\n\n");
+}
+
+int obtenerMayor(int vec[], int tam){
+    int max;
+    int flag = 0;
+
+        for(int i=0; i<5; i++){
+            if(max < vec[i] || flag == 0){
+                max = vec[i];
+                flag = 1;
+            }
+        }
+
+    return max;
+}
