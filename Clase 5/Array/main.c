7a2fb81554483f3f144ae804c6abae71d9c2edd7
alumno <alumno>
2019-09-09 21:29:08 -0300

    falta arreglar clase 5

--- Clase 5/Array/main.c
+++ Clase 5/Array/main.c
@@ -1,44 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAM 5
+#define TAM 3
 
+void pedirVector(int vec[], int tam);
 void mostrarVectorInt(int vec[], int tam);
+int sumarVector(int vec[], int tam);
 int obtenerMayor(int vec[], int tam);
-void obtenerIndice(int vec[]n int tam, int num);
+int obtenerIndice(int vec[], int tam, int num);
 
 
 int main()
 {
-    int numeros[5];
-    int suma = 0;
+    int numeros[TAM];
+    int suma;
     int max;
     int index;
 
-    for(int i = 0; i < 5; i++){
-        printf("Ingrese un numero: ");
-        scanf("%d", &numeros[i]);
-    }
-    mostrarVectorInt(numeros, 5);
-    /*
-    for(int i = 0; i < 5; i++){
-        printf(" %d ", numeros[i]);
-        if(max <= numeros[i]){
-                max = numeros[i];
-                index = i;
-            }
-    }*/
-    max = obtenerMayor(numeros, 5);
-    obtenerIndice(numeros, TAM, max)
+    pedirVector(numeros, TAM);
+    mostrarVectorInt(numeros, TAM);
+    max = obtenerMayor(numeros, TAM);
+    index = obtenerIndice(numeros, TAM, max);
+    suma = sumarVector(numeros, TAM);
 
-    for(int i = 0; i < 5; i++){
-        suma += numeros[i];
-    }
-        printf("\n la suma es: %d", suma);
+    printf("\n la suma es: %d", suma);
 
-        printf("\n el maximo es %d y el indice es %d", max, index);
+    printf("\n el maximo es %d y el indice es %d", max, index);
 
     return 0;
+}// FIN MAIN
+
+void pedirVector(int vec[], int tam){
+    for(int i = 0; i < tam; i++){
+        printf("Ingrese un numero: ");
+        scanf("%d", &vec[i]);
+    }
 }
 
 void mostrarVectorInt(int vec[], int tam){
@@ -49,11 +45,19 @@ void mostrarVectorInt(int vec[], int tam){
     printf("\n\n");
 }
 
+int sumarVector(int vec[], int tam){
+    int suma = 0;
+     for(int i = 0; i < tam; i++){
+            suma =  suma + vec[i];
+        }
+    return suma;
+}
+
 int obtenerMayor(int vec[], int tam){
     int max;
     int flag = 0;
 
-        for(int i=0; i<5; i++){
+        for(int i=0; i<tam; i++){
             if(max < vec[i] || flag == 0){
                 max = vec[i];
                 flag = 1;
@@ -62,12 +66,12 @@ int obtenerMayor(int vec[], int tam){
     return max;
 }
 
-void obtenerIndice(int vec[] int tam, int num){
+int obtenerIndice(int vec[], int tam, int num){
     int index;
      for(int i=0; i<tam; i++){
         if( vec[i] = num){
             index = i;
+            break;
         }
-
-
+ }
 }
